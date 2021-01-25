df425cab04adc450a038ce903abafe5296ec735c
Arun Sivasankaran <arunparthiban@gmail.com>
2021-01-24 17:15:06 -0800

    feat: better implementation

--- caesarcipher/caesarcipher.go
+++ caesarcipher/caesarcipher.go
@@ -1,31 +1,67 @@
 package caesarcipher
 
-import "strings"
+const alphabet =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-const plain =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const cipher =   "XYZABCDEFGHIJKLMNOPQRSTUVW"
+type caesarCipherBuilder struct {
+	plainTextRuneValueMap  map[rune]int
+	plainTextValueRuneMap map[int]rune
+	cipherTextValueRuneMap map[int]rune
+	cipherTextRuneValueMap map[rune]int
+}
 
-func Encrypt(input string) string {
+
+func Init(shift int) caesarCipherBuilder {
+
+	var ccb caesarCipherBuilder
+
+	var plainTextRuneValueMap = make(map[rune]int)
+	var plainTextValueRuneMap = make(map[int]rune)
+
+	var cipherTextValueRuneMap = make(map[int]rune)
+	var cipherTextRuneValueMap = make(map[rune]int)
+
+	for i,v := range alphabet {
+		plainTextRuneValueMap[v] = i + 1
+		plainTextValueRuneMap[i+1] = v
+		newIndex := i + shift
+		newRune := rune(alphabet[newIndex % 26])
+		cipherTextValueRuneMap[i+1] = newRune
+		cipherTextRuneValueMap[newRune] = i+1
+	}
+
+	ccb.plainTextRuneValueMap = plainTextRuneValueMap
+	ccb.cipherTextValueRuneMap = cipherTextValueRuneMap
+	ccb.plainTextValueRuneMap = plainTextValueRuneMap
+	ccb.cipherTextRuneValueMap = cipherTextRuneValueMap
+
+	//fmt.Println(plainTextRuneValueMap)
+	//fmt.Println(cipherTextValueRuneMap)
+	//
+	//fmt.Println(plainTextValueRuneMap)
+	//fmt.Println(cipherTextRuneValueMap)
+	return ccb
+}
+
+
+func (ccb *caesarCipherBuilder) Encrypt(input string) string {
 
 	var output string
 
 	for _, char := range input {
-		i := strings.Index(plain, string(char))
-		newI := cipher[i]
-		output = output + string(newI)
+		newRune := ccb.cipherTextValueRuneMap[(ccb.plainTextRuneValueMap[char])]
+		output = output + string(newRune)
 	}
 
 	return output
 }
 
-func Decrypt(input string) string {
+func (ccb *caesarCipherBuilder) Decrypt(input string) string {
 
 	var output string
 
 	for _, char := range input {
-		i := strings.Index(cipher, string(char))
-		newI := plain[i]
-		output = output + string(newI)
+		newRune := ccb.plainTextValueRuneMap[(ccb.cipherTextRuneValueMap[char])]
+		output = output + string(newRune)
 	}
 
 	return output
