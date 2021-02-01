748187a8e329a5465004575b8c125d0f28ef520f
Arun Sivasankaran <arunparthiban@gmail.com>
2021-01-31 23:49:26 -0800

    feat: run some profiling code and run go fmt

--- caesarcipher/caesarcipher_test.go
+++ caesarcipher/caesarcipher_test.go
@@ -9,9 +9,7 @@ import (
 var cb = Init(5)
 
 func TestEncrypt(t *testing.T) {
-
 	output := cb.Encrypt("HELLO")
-
 	if output != "MJQQT" {
 		t.Error("expected MJQQT, but got ", output)
 	}
@@ -19,18 +17,14 @@ func TestEncrypt(t *testing.T) {
 }
 
 func TestDecrypt(t *testing.T) {
-
 	output := cb.Decrypt("MJQQT")
-
 	if output != "HELLO" {
 		t.Error("expected HELLO, but got ", output)
 	}
 }
 
 func BenchmarkEncrypt(b *testing.B) {
-
 	f := faker.New()
-
 	for i := 0; i < b.N; i++ {
 		cb.Encrypt(f.Lorem().Paragraph(10))
 	}
