9e35ae8e2dcd86d233203dacfa8c38467053ce4c
Arun Sivasankaran <arunparthiban@gmail.com>
2021-02-01 00:48:47 -0800

    feat: add better tests and benchmarks (#6)
    * feat: add better tests and benchmarks
    
    * feat: run some profiling code and run go fmt
    
    * feat: delete test binary

--- caesarcipher/caesarcipher_test.go
+++ caesarcipher/caesarcipher_test.go
@@ -1,21 +1,31 @@
 package caesarcipher
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/jaswdr/faker"
+)
+
+var cb = Init(5)
 
 func TestEncrypt(t *testing.T) {
-
-	cb := Init(5)
-
 	output := cb.Encrypt("HELLO")
-
 	if output != "MJQQT" {
 		t.Error("expected MJQQT, but got ", output)
 	}
 
-	output = cb.Decrypt("MJQQT")
+}
 
+func TestDecrypt(t *testing.T) {
+	output := cb.Decrypt("MJQQT")
 	if output != "HELLO" {
 		t.Error("expected HELLO, but got ", output)
 	}
+}
 
+func BenchmarkEncrypt(b *testing.B) {
+	f := faker.New()
+	for i := 0; i < b.N; i++ {
+		cb.Encrypt(f.Lorem().Paragraph(10))
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Mount-Myoboku/crypto
 
 go 1.15
+
+require github.com/jaswdr/faker v1.3.0
