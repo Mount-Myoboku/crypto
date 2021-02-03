36703b537d867faeab48773517a6f8833461e9c7
Arun Sivasankaran <arunparthiban@gmail.com>
2021-02-02 22:58:00 -0800

    feat: use a simpler api for caesar cipher (#7)
    * feat: use a simpler api for caesar cipher
    
    * feat: better tests
    
    * feat: add examples and benchmark functions
    
    * feat: remove vscode stuff
    
    * feat: add new version

--- caesarcipher/caesarcipher_test.go
+++ caesarcipher/caesarcipher_test.go
@@ -1,31 +1,72 @@
 package caesarcipher
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/jaswdr/faker"
 )
 
-var cb = Init(5)
+type testCase struct {
+	input  string
+	output string
+}
 
 func TestEncrypt(t *testing.T) {
-	output := cb.Encrypt("HELLO")
-	if output != "MJQQT" {
-		t.Error("expected MJQQT, but got ", output)
+
+	cases := []testCase{
+		{input: "HELLO", output: "EBIIL"},
+		{input: "!*@{,", output: "!*@{,"},
+		{input: "ZAZZY", output: "WXWWV"},
+	}
+
+	for _, test := range cases {
+		output := Encrypt(test.input)
+		if output != test.output {
+			t.Errorf("expected %s, but got %s", test.output, output)
+		}
 	}
 
 }
 
 func TestDecrypt(t *testing.T) {
-	output := cb.Decrypt("MJQQT")
-	if output != "HELLO" {
-		t.Error("expected HELLO, but got ", output)
+
+	cases := []testCase{
+		{output: "HELLO", input: "EBIIL"},
+		{output: "!*@{,", input: "!*@{,"},
+		{output: "ZAZZY", input: "WXWWV"},
+	}
+
+	for _, test := range cases {
+		output := Decrypt(test.input)
+		if output != test.output {
+			t.Errorf("expected %s, but got %s", test.output, output)
+		}
 	}
 }
 
 func BenchmarkEncrypt(b *testing.B) {
 	f := faker.New()
 	for i := 0; i < b.N; i++ {
-		cb.Encrypt(f.Lorem().Paragraph(10))
+		Encrypt(f.Lorem().Paragraph(10))
 	}
+}
+
+func BenchmarkDecrypt(b *testing.B) {
+	f := faker.New()
+	for i := 0; i < b.N; i++ {
+		Decrypt(f.Lorem().Paragraph(10))
+	}
+}
+
+func ExampleEncrypt() {
+	output := Encrypt("HELLO!")
+	fmt.Println(output)
+	// "EBIIL!"
+}
+
+func ExampleDecrypt() {
+	output := Encrypt("EBIIL!")
+	fmt.Println(output)
+	// "HELLO!"
 }
